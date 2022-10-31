d78cbdd4bbbafd3c16fb0b645c0164ae17ddc76b
showa_yojyo <yojyo@hotmail.com>
2022-10-31 22:41:14 +0900

    src/selectdata.py: add a script querying data

---
+++ src/selectdata.py
@@ -0,0 +1,48 @@
+import sys
+from connection import create_connection
+
+
+def select_all_tasks(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def select_task_by_priority(conn, priority):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks WHERE priority=?", (priority,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def main(database):
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        print("1. Query task by priority:")
+        select_task_by_priority(conn, 1)
+
+        print("2. Query all tasks")
+        select_all_tasks(conn)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
