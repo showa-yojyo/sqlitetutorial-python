eaa208ff352170bab127e832c4f948843db81a67
showa_yojyo <yojyo@hotmail.com>
2022-10-31 22:23:53 +0900

    src/connection.py: move create_connection to a new module

--- src/createtable.py
+++ src/createtable.py
@@ -1,22 +1,6 @@
 import sys
-import sqlite3
 from sqlite3 import Error
-
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-
-    return conn
+from connection import create_connection
 
 
 def create_table(conn, create_table_sql):
