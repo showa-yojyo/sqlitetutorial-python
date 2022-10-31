369b0f8fcd49ef10ccee4911474ed585e7613af6
showa_yojyo <yojyo@hotmail.com>
2022-10-31 22:30:04 +0900

    src/updatedata.py: add a script updating data

---
+++ src/updatedata.py
@@ -0,0 +1,29 @@
+import sys
+from connection import create_connection
+
+def update_task(conn, task):
+    """
+    update priority, begin_date, and end date of a task
+    :param conn:
+    :param task:
+    :return: project id
+    """
+    sql = ''' UPDATE tasks
+              SET priority = ? ,
+                  begin_date = ? ,
+                  end_date = ?
+              WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+
+def main(database):
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        update_task(conn, (2, '2015-01-04', '2015-01-06', 2))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
